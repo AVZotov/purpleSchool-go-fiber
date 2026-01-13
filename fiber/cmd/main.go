769f7e545578c62c6623576b128a5b4e67bc0e7a
Alexey Zotov <alexey1.zotov@yandex.ru>
2026-01-13 18:13:40 +0300

    init routing added

--- fiber/cmd/main.go
+++ fiber/cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-fiber/internal/home"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	fmt.Println("Start")
+	app := fiber.New()
+
+	home.NewHandler(app)
+
+	app.Listen(":3000")
 }
---
+++ fiber/internal/home/hadler.go
@@ -0,0 +1,19 @@
+package home
+
+import "github.com/gofiber/fiber/v2"
+
+type Handler struct {
+	router fiber.Router
+}
+
+func NewHandler(router fiber.Router) {
+	h := &Handler{
+		router: router,
+	}
+
+	h.router.Get("/", h.home)
+}
+
+func (h *Handler) home(c *fiber.Ctx) error {
+	return c.SendString("Home")
+}
